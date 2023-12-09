2827d6de936b0d3df4066f0e9694ea0fbc1619ed
Dênis Araújo da Silva <silvadenisaraujo@gmail.com>
2023-12-09 12:06:19 +0100

    Respond with 200

--- app/server.go
+++ app/server.go
@@ -13,10 +13,23 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	
-	_, err = l.Accept()
+
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
+	}
+
+	// Keep the connection open until the application closes
+	defer conn.Close()
+
+	fmt.Println("Connection successful")
+	// Return a 200 response
+	response := "HTTP/1.1 200 OK\r\n\r\n"
+
+	// Send the response
+	_, err = conn.Write([]byte(response))
+	if err != nil {
+		fmt.Println("Error sending response: ", err.Error())
 	}
 }
